7b2a5be4034f911fecbfac4041aa8383a93074c2
nguoianphu <nguoianphu@gmail.com>
2019-03-31 18:51:41 +0700

    init

---
+++ build-android.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -v
+
+set -e
+
+echo "Adding Platforms"
+cordova platform add android
+# cordova platforms ls
+
+echo "Adding Plugins"
+cordova plugin add cordova-plugin-whitelist
+cordova plugin ls
+
+echo "Building apk for Android"
+cordova build android --release
+
+echo "Built the following apk(s):"
+cp -R $TRAVIS_BUILD_DIR/platforms/android/app/build/outputs/apk/release/* $TRAVIS_BUILD_DIR/
+cd $TRAVIS_BUILD_DIR
+ls -la
+
+echo "Signing an App"
+
+echo "Generate a Private Certificate by keytool"
+keytool -genkey -v -noprompt \
+ -alias android-app-key \
+ -keystore android.keystore \
+ -keyalg RSA -keysize 2048 -validity 10000 \
+ -storepass ${MY_ZIP_PASSWORD} \
+ -keypass ${MY_ZIP_PASSWORD} \
+ -dname "CN=nguoianphu.com, OU=NA, O=Company, L=HOCHIMINH, S=HOCHIMINH, C=VN"
+
+echo "Sign the APK with the key we just created"
+jarsigner -verbose \
+    -sigalg SHA1withRSA \
+    -digestalg SHA1 \
+    -storepass ${MY_ZIP_PASSWORD} \
+    -keystore android.keystore app-release-unsigned.apk android-app-key
+
+echo "Optimize the APK file with the zipalign tool and also rename it to reflect the signing."
+cd /usr/local/android-sdk/build-tools/${BUILD_TOOLS_VERSION}/
+./zipalign -v 4 $TRAVIS_BUILD_DIR/app-release-unsigned.apk $TRAVIS_BUILD_DIR/app-release.apk
+ls -la
+cd $TRAVIS_BUILD_DIR/
+ls -la
